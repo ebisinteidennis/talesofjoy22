ec2ad69807f605a6f736f5c3ec1638077f4aa491
ebisinteidennis <ebisinteidennis@gmail.com>
2022-11-06 12:56:59 +0100

    Third commit

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 2022,
+                month: 12,
+                day: 9,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
