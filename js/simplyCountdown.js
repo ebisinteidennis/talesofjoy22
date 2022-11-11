8d65a9ac0c1f1a7e71f84a0fa8978c5daf2d1807
Ebisintei Dennis <85201342+ebisinteidennis@users.noreply.github.com>
2022-11-11 09:06:54 -0800

    Update simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -13,7 +13,7 @@ var x = setInterval(function() {
     var days = Math.floor(distance / (1000 * 60 * 60 * 24))
     var hours = Math.floor(distance % ((1000*60*60*24))/(1000*60*60));
     var minutes = Math.floor(distance % (1000*60*60))/ (1000*60));
-    var seconds =  Math.floor(distance % (1000*60*60))/ (1000*60));
-  
-
+    var seconds =  Math.floor(distance % (1000*60))/ (1000*60));
+    
+    //output the result in the element 
 }
